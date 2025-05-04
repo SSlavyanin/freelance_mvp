224c0913fb580dc608a7b1e59c9fe0a70e5df9e1
SSlavyanin <156539470+SSlavyanin@users.noreply.github.com>
2025-05-04 17:35:00 +0300

    Create config.py

---
+++ shared/config.py
@@ -0,0 +1,6 @@
+# shared/config.py
+
+TELEGRAM_BOT_TOKEN = "your-telegram-bot-token"
+PARSE_INTERVAL_SECONDS = 300  # каждые 5 минут
+PARSER_URL = "http://localhost:3000/parse"
+TELEGRAM_USERNAME = "@yourusername"  # для автооткликов
